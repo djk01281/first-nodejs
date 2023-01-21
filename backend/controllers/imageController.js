26df5747300db12102ef251c67d33d35d3d95188
djk01281 <djk01281@gmail.com>
2023-01-21 05:48:07 +0000

    feat: add random-image API

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -1,15 +1,18 @@
 const {allIds, findById} = require('../models/imageModel')
 const fs = require('fs')
 
+
 async function getRandomImage(req, res, params){
     try{
         const ids = await allIds(params[0])
+        console.log(`ids: ${ids}`)
+
         const randomIndex = Math.floor(Math.random() * ids.length)
         const randomId = ids[randomIndex]
 
-        const image = await findById(params[0], randomId)
-        res.writeHead(200, {'Content-Type': 'image/jpeg'})
-        res.end(JSON.stringify(image))
+        console.log(`randomId: ${randomId}`)
+        await findById(params[0], randomId, (type, data) => {res.writeHead(200, {'Content-Type': type})
+        res.end(data)})
     }
 
     catch(error){console.log(error)}
--- backend/models/imageModel.js
+++ backend/models/imageModel.js
@@ -1,20 +1,33 @@
-const Catimages = require('../data/catImages.json')
-const Dogimages = require('../data/catImages.json')
-
-const Images = {"cat": Catimages, "dog": Dogimages}
-
+const path = require('path')
 const fs = require('fs')
 
-const findById = (animal, id) => {
-    return new Promise((resolve, reject) =>{
-        fs.createReadStream(Catimages)
-        resolve(Catimages)
+const findById = (animal, id, callback) => {
+    return new Promise(async(resolve, reject) =>{
+        const imgPath = path.join(__dirname, `../data/${animal}/${id}`)
+
+        await fs.readFile(imgPath, (err, data) => {
+            if(err){ console.log(err)}
+            else{callback("image/jpeg", data)}
+        })
+
+        resolve("Good!")
     })
 }
 
 const allIds = (animal) => {
     return new Promise ((resolve, reject) => {
-        resolve(Object.keys(Images[animal]))
+        const imgPath = `../data/${animal}`
+        const fullPath = path.join(__dirname, imgPath)
+        console.log(fullPath)
+        fs.readdir(fullPath, (err, files) => {
+            if(err){
+                console.log(err)
+            }
+            else{
+            console.log(files)
+            resolve(files)
+            }
+        })        
     })
 }
 
